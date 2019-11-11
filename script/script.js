e902d81308fb97757e6f51340090036142d9bc7b
Vyacheslav Vojdaev <parselian.study@yandex.ru>
2019-11-11 18:17:08 +0300

    сделал основу 13-го урока

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-  function timer(deadline, hour, minute, second) {
+  const timer = (deadline, hour, minute, second) => {
     const timerHours = document.querySelector(hour),
           timerMinutes = document.querySelector(minute),
           timerSeconds = document.querySelector(second);
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
     //   deadline = new Date().getTime() + 86400000;
     // }
 
-    function calcTime() {
+    const calcTime = () => {
       let currDate = new Date().getTime(),
           endTime = new Date(deadline).getTime(),
           timeRemaining = (endTime - currDate) / 1000,
@@ -20,7 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
       return {seconds, minutes, hours, timeRemaining};
     }
 
-    function updateClock() {
+    const updateClock = () => {
       let timer = calcTime();
       
       timerHours.textContent = timer.hours;
@@ -40,21 +40,81 @@ window.addEventListener('DOMContentLoaded', () => {
       
       if( timer.timeRemaining <= 0 ) {
         deadline = new Date().getTime() + 86400000;
-        intervalId = setInterval(updateClock, 1000 );
+        intervalId = setInterval(updateClock(), 1000 );
         
         timerHours.textContent = '00';
         timerMinutes.textContent = '00';
         timerSeconds.textContent = '00';
       }
       
-      
+
 
     }
     
     let intervalId = setInterval(updateClock, 1000);
 
     intervalId;
-  }
+  };
 
-timer('7 november 2019', '#timer-hours', '#timer-minutes', '#timer-seconds');
+  timer('17 november 2019', '#timer-hours', '#timer-minutes', '#timer-seconds');
+
+
+
+  // Меню
+  const menuBtn = document.querySelector('.menu'),
+        menu = document.querySelector('menu'),
+        closeBtn = document.querySelector('.close-btn'),
+        menuItems = menu.querySelectorAll('ul > li > a');
+
+  const toggleMenu = () => {
+    if( !menu.style.transform || menu.style.transform === 'translateX(-100%)' ) {
+      menu.style.transform = `translateX(0)`;
+    } else {
+      menu.style.transform = `translateX(-100%)`;
+    }
+  };
+
+  menuBtn.addEventListener('click', toggleMenu);
+  closeBtn.addEventListener('click',toggleMenu);
+  menuItems.forEach((item) => item.addEventListener('click', toggleMenu));
+
+
+
+  // Попап
+  const popup = document.querySelector('.popup'),
+        popupClose = popup.querySelector('.popup-close'),
+        popupBtn = document.querySelectorAll('.popup-btn');
+
+  const animatePopup = () => {
+    let intervalId,
+        counter = 0;
+
+    intervalId = setInterval(() => {
+      popup.style.opacity = counter;
+      counter += 0.1;
+    }, 10);
+    
+    if (counter > 1 || screen.width < 768) {
+      clearInterval(intervalId);
+      popup.style.opacity = 1;
+    }
+    
+    popup.style.display = 'block';
+  };
+  
+  popupBtn.forEach( (item) => {
+    item.addEventListener('click', () => {
+      popup.style.opacity = 0;
+      animatePopup();
+    });
+  });
+
+  popupClose.addEventListener('click', () => {
+    popup.style.display = 'none'
+  });
+
+
+  if(screen.width < 768) {
+    console.dir(screen.width);
+  }
 });
