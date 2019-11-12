5a3b649fa0d83e2b3ca021eb8959a3ab85a749ec
Vyacheslav Vojdaev <parselian.study@yandex.ru>
2019-11-12 23:20:08 +0300

    сделал усложненку 14-го урока
    й

--- script/script.js
+++ script/script.js
@@ -66,31 +66,60 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const toggleMenu = () => {
 
-    header.addEventListener('click', (e) => {
+    // header.addEventListener('click', (e) => {
+    //   let target = e.target;
+    //       target = target.closest('.menu');
+      
+    //   if(target) {
+    //     if( !menu.style.transform || menu.style.transform === 'translateX(-100%)' ) {
+    //       menu.style.transform = `translateX(0)`;
+    //     } else {
+    //       menu.style.transform = `translateX(-100%)`;
+    //     }
+    //   }
+    // });
+        
+    // menu.addEventListener('click', (e) => {
+    //   let target = e.target;
+          
+    //   if(target.matches('.close-btn')) {
+    //     menu.style.transform = `translateX(-100%)`;
+    //   } else {
+    //     target = target.closest('a');
+
+    //     if(target) {
+    //       menu.style.transform = `translateX(-100%)`;
+    //     }
+    //   }
+    // });
+
+    window.addEventListener('click', (e) => {
       let target = e.target;
           target = target.closest('.menu');
-      
+
       if(target) {
         if( !menu.style.transform || menu.style.transform === 'translateX(-100%)' ) {
           menu.style.transform = `translateX(0)`;
         } else {
           menu.style.transform = `translateX(-100%)`;
         }
-      }
-    });
-        
-    menu.addEventListener('click', (e) => {
-      let target = e.target;
-          
-      if(target.matches('.close-btn')) {
-        menu.style.transform = `translateX(-100%)`;
-      } else {
-        target = target.closest('a');
+      } if(!target) {
+        target = e.target;
 
-        if(target) {
+        if(target.classList.contains('close-btn') && !target.matches('menu')) {
+          menu.style.transform = `translateX(-100%)`;
+        } else if(target.matches('a') && !target.matches('menu')) {
           menu.style.transform = `translateX(-100%)`;
         }
+        else {
+          target = target.closest('menu');
+  
+          if(!target) {
+            menu.style.transform = `translateX(-100%)`;
+          } 
+        }
       }
+
     });
 };
 
