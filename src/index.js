e2e53d26643d9fb6f4c5fec738fa4a2d2a234652
Vyacheslav Vojdaev <parselian.study@yandex.ru>
2019-11-26 23:18:08 +0300

    добавил валидатор формы

--- src/index.js
+++ src/index.js
@@ -21,6 +21,8 @@ import calcValidation from './modules/calcValidation';
 import calculator from './modules/calculator';
 import sendForm from './modules/sendForm';
 import validationForm from './modules/validationForm';
+import Validator from './modules/validator';
+
 
 //таймер
 timer('17 november 2019', '#timer-hours', '#timer-minutes', '#timer-seconds');
@@ -66,7 +68,80 @@ const options = {
   }
   ]
 };
-
 const carousel = new SliderCarousel(options);
-
 carousel.init();
+
+const valid1 = new Validator({
+  selector: '#form1',
+  pattern: {
+    phone: /^\+380\d{7}$/,
+    text: /^[а-яА-Я]{1,}$/
+  },
+  method: {
+    'form1-phone': [
+      ['notEmpty'],
+      ['pattern', 'phone']
+    ],
+    'form1-email': [
+      ['notEmpty'],
+      ['pattern', 'email']
+    ],
+    'form1-name': [
+      ['notEmpty'],
+      ['pattern', 'text']
+    ]
+  }
+});
+
+const valid2 = new Validator({
+  selector: '#form2',
+  pattern: {
+    phone: /^\+380\d{7}$/,
+    text: /^[а-яА-Я]{1,}$/
+  },
+  method: {
+    'form2-phone': [
+      ['notEmpty'],
+      ['pattern', 'phone']
+    ],
+    'form2-email': [
+      ['notEmpty'],
+      ['pattern', 'email']
+    ],
+    'form2-name': [
+      ['notEmpty'],
+      ['pattern', 'text']
+    ],
+    'form2-message': [
+      ['notEmpty'],
+      ['pattern', 'text']
+    ]
+  }
+});
+
+const valid3 = new Validator({
+  selector: '#form3',
+  pattern: {
+    phone: /^\+380\d{7}$/,
+    text: /^[а-яА-Я]{1,}$/
+  },
+  method: {
+    'form3-phone': [
+      ['notEmpty'],
+      ['pattern', 'phone']
+    ],
+    'form3-email': [
+      ['notEmpty'],
+      ['pattern', 'email']
+    ],
+    'form3-name': [
+      ['notEmpty'],
+      ['pattern', 'text']
+    ]
+  }
+});
+
+valid1.init();
+valid2.init();
+valid3.init();
+
